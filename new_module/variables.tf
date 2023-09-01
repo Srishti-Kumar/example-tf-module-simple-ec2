4bcda9a22735b9ddf9af9cc8eaba6c60cca8119f
Srishti <75185963+Srishti-Kumar@users.noreply.github.com>
2023-09-01 17:34:51 +0530

    Update variables.tf

--- new_module/variables.tf
+++ new_module/variables.tf
@@ -1,17 +1,21 @@
 variable "ec2_instance_type" {
   type    = string
   default = "t2.micro"
+  description = "this is an instance type / size to use"
 }
 
 variable "ec2_instance_name" {
 
   type = string
+  description = "to give name to instance"
 }
 
 variable "ec2_ami_id" {
   type = string
+  description = "ami id used to launch ec2 instance, these differ with in regions"
 }
 
 variable "number_of_instances" {
   type = number
+  description = "number of instances to launch"
 }
